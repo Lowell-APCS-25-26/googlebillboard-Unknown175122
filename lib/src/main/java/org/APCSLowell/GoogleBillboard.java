86183e40dab3d36e10671fef268d57cff2bbbdd3
Unknown175122 <fskelliu@gmail.com>
2026-01-09 18:08:00 +0000

    modified:   lib/src/main/java/org/APCSLowell/GoogleBillboard.java

--- lib/src/main/java/org/APCSLowell/GoogleBillboard.java
+++ lib/src/main/java/org/APCSLowell/GoogleBillboard.java
@@ -4,16 +4,25 @@
 package org.APCSLowell;
 
 public class GoogleBillboard {
+    //public final static String e = "27182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
     public boolean isPrime(long n) {
-        for (int i = 2; i < Math.sqrt(dNum); i++){
-            if (dNum%i == 0){
+        for (int i = 2; i <= Math.sqrt(n); i++){
+            if (n%i == 0){
                 return false;
             }
         }
-        return dNum > 1;
+        return n > 1;
     }
     public long firstPrimeNumberIn(String n) {
-        // Write your code here.
+        long d = 0;
+        int count = 0;
+        for (int i = 0; i < n.length(); i++){
+            if (isPrime(d)){
+                return d;
+            }
+        d = Long.parseLong(n.substring(count, count+10));
+        count++;
+        }
         return -1;
     }
 }
